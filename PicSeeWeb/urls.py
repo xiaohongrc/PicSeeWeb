c93f40e5be12807fe2c7c5a753c1b9cf9c977854
hongenit_pro <hongenit@hongenit-prodeMacBook-Pro.local>
2018-04-20 17:27:20 +0800

    Initial commit

---
+++ PicSeeWeb/urls.py
@@ -0,0 +1,25 @@
+"""PicSeeWeb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from web_api.views import get_classify_list, get_album_info_list
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('get_classify_list', get_classify_list),
+    path('get_album_info_list/', get_album_info_list),
+]
---
+++ web_api/views.py
@@ -0,0 +1,36 @@
+import pymongo as pymongo
+from django.shortcuts import render
+from bson.json_util import dumps
+from django.http import HttpResponse
+
+# Create your views here.
+
+client = pymongo.MongoClient('localhost', 27017)
+db_win_400 = client['win400']
+# 分类的url数据库
+sheet_classify = db_win_400['sheet_classify']
+# 分类下的图集列表数据库
+sheet_album_list = db_win_400['sheet_album_list']
+# 单个图集的信息数据库
+sheet_album_info = db_win_400['sheet_album_info']
+
+
+# 获取分类标题列表
+def get_classify_list():
+    return
+
+
+# 获取图集信息列表
+def get_album_info_list(request):
+    width = request.GET.get('width', None)
+    albumUrl = request.GET.get('albumUrl', None)
+    result = [{"title": "titlename", "age": 3}]
+    # for item in sheet_album_info.find({'albumUrl':albumUrl}):
+    #     result.append(item)
+    print(result)
+    return HttpResponse(dumps(result), content_type="application/json")
+
+# result = []
+# for item in sheet_album_info.find({'width': '720'}):
+#     result.append(item)
+# print(result)
